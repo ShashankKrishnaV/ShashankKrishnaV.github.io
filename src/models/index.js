12bd2042edf792c3cad1e8f3861a6f65f24db7e2
Shashank Krishna Vempati <vempatishashankbzy@gmail.com>
2024-12-28 17:59:49 +0530

    Add files via upload

---
+++ src/models/index.js
@@ -0,0 +1,6 @@
+export { Galaxy } from "./Galaxy";
+export { Bird } from './Bird'
+export { Plane } from "./Plane";
+export { Island } from "./Island";
+export { Fox } from "./Fox"
+export { Earth } from "./Earth"
